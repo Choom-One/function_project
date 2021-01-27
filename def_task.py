c0a1427eaa08eab2997b43344001108819cca48a
Choom <onezedov@gmail.com>
2021-01-27 23:13:00 +0300

    Complete task

---
+++ def_task.py
@@ -0,0 +1,10 @@
+# def name(n):
+#     print(f'Hello {n}')
+# name('Artiom')
+
+list_name = ['vasya', 'vova', 'kirill']
+def name(n):
+    for i in list_name:
+        print(i)
+
+name(list_name)
---
+++ func_lesson.py
@@ -0,0 +1,6 @@
+def my_func(a, b):
+    summ = a + b
+    print(f'{a} + {b} = {summ}')
+    return summ
+a = my_func(5, 5)
+print(a)
