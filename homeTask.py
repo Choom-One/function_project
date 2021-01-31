c9647021cea3f0af291d318970116184c367638b
Choom <onezedov@gmail.com>
2021-01-31 12:20:58 +0300

    f2 complete

--- homeTask.py
+++ homeTask.py
@@ -1,4 +1,9 @@
-def Func(a):
-    a *= 2
-    return a
-print (Func(float(input('input km: '))))
+def km_ml(x):
+    x *= 0.621371
+    return x
+print (km_ml(float(input('input km: '))))
+
+def ml_km(q):
+    q *= 1.60934
+    return q
+print(ml_km(float(input('input ml: '))))
